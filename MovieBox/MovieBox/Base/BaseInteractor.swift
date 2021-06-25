80a56c6a10b406ddc074d928f0bf480061ea1526
Önder Murat Erol <ondermerol@gmail.com>
2021-06-25 21:48:42 +0300

    Alamofire And TMDB API Calls
    The structure is not correct, just successfull api calls

---
+++ MovieBox/MovieBox/Base/BaseInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  BaseInteractor.swift
+//  MovieBox
+//
+//  Created by Onder on 25.06.2021.
+//
+
+import Alamofire
+import UIKit
+
+class BaseInteractor: NSObject {
+    // func callService<T, Codable>(withRequest request:
+}
