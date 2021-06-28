07cb850ace1878f575ce018847c11e25031fde30
Önder Murat Erol <ondermerol@gmail.com>
2021-06-28 14:37:42 +0300

    Unit test initial build problems were fixed.

---
+++ MovieBox/MovieBoxTests/Dashboard/DashboardViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  DashboardViewControllerTests.swift
+//  MovieBoxTests
+//
+//  Created by Wolverin Mm on 28.06.2021.
+//
+
+import XCTest
+
+class DashboardViewControllerTests: XCTestCase {
+
+
+
+}
